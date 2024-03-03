9979defce5dc3f953b56ed828af93cdbf541b8d0
ZOYA ARMAN <KausharAnsari2002>
2024-03-02 21:29:19 -0800

    mobarak

---
+++ DataType.js
@@ -0,0 +1,18 @@
+let age = 25;
+let price = 100.99; 
+// this is a number type
+
+
+
+let fullname = "kaushar";
+// this is a string type
+
+
+ let isfollow = false;
+//   this is a Boolean
+
+let x;
+// this is a  undifine
+
+let r =  null;
+// this is a object
---
+++ Vairiable.js
@@ -0,0 +1,35 @@
+fullname = ("tony star");
+console.log(fullname);
+age = 20;
+console.log(age);
+price = 9999.00;
+console.log(price);
+x = null;
+console.log(x);
+Isfollow = true;
+console.log(Isfollow);
+fullname = 25;
+fullname = "zoya"
+console.log(fullname);
+
+
+
+let name = "kaushar bhai";
+console.log(name);
+
+let totalprice= 99.99;
+console.log(totalprice);
+ let a;
+ a = 50;
+ console.log(a);
+
+{
+    let a = 45;
+ console.log(a);
+
+
+}
+ const b = 45;
+ console.log(b);
+
+
